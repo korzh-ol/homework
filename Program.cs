d4cb20586d171b959300cec8277da8eb46027f4a
korzh-ol <72135720+korzh-ol@users.noreply.github.com>
2020-10-14 11:31:33 +0500

    Create Program.cs
    Корж Ольга, УГИ-106004

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace integerArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Введите количество сотен, десятков и единиц в трёхзначном числе:");
+
+            int sotni = Convert.ToInt32(Console.ReadLine());
+            
+            int desyatki = Convert.ToInt32(Console.ReadLine());
+
+            int edinici = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Введённое число равняется " + sotni + desyatki + edinici);
+
+            Console.WriteLine("Число после перестановки цифр десятков и единиц равно " + sotni + edinici + desyatki);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}

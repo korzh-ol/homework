849c1c755d7baf075e1d2411d7587565a67ddbd2
korzh-ol <72135720+korzh-ol@users.noreply.github.com>
2020-12-28 12:40:50 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace zachet_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserPhrase = Console.ReadLine();
+            UserPhrase.ToLower();
+
+            char[] Vowels = { 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я' };
+
+            int VowelSum = 0;
+            for (int i = 0; i < Vowels.Length; i++)
+            {
+                if (UserPhrase.Contains(Vowels[i]))
+                {
+                    Console.WriteLine($"{Vowels[i]} - {UserPhrase.Split(Vowels[i]).Length - 1}");
+                    VowelSum += UserPhrase.Split(Vowels[i]).Length - 1;
+                }
+                
+            }
+            Console.WriteLine($"Всего гласных - {VowelSum}");
+            Console.ReadKey();
+        }
+    }
+}
